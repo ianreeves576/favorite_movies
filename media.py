e31b0de6cfcce60f22715a3d8f1bf88b96dbc72d
ianreeves576 <ianreeves576@hotmail.com>
2017-07-03 12:44:55 -0700

    Add files via upload

--- media.py
+++ media.py
@@ -1,8 +1,9 @@
 import webbrowser
 
+
 class Movie():
     """This class provides a way to store movie related information.
-        
+
     Attributes:
         movie_title: The title of the movie.
         movie_storyline: Short summary of the movie storyline.
@@ -10,7 +11,8 @@ class Movie():
         trailer_youtube: A URL to the movie trailer.
     """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         """Inits a movie class with information about the movie"""
         self.title = movie_title
         self.storyline = movie_storyline
@@ -18,5 +20,6 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        """This function opens a new browser and plays the trailer for a movie instance.""" 
+        """This function opens a new browser and plays the
+           trailer for a movie instance."""
         webbrowser.open(self.trailer_youtube_url)
